21f99d3a0a91002091f195e94040e971b2947c38
Warren Chan <warrenchan@warren-2.hitronhub.home>
2018-03-04 22:28:19 -0800

    Added prismic CMS.

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
+import Prismic from 'prismic-javascript';
 import styles from './styles.scss'
 
 import photo from '../../assets/photos/_MG_9081.jpg'
 
 class Jumbotron extends Component {
-  state = {  }
+  state = {
+    doc: null,
+  }
+
+  componentWillMount() {
+    const apiEndpoint = 'https://warrensite.prismic.io/api/v2';
+    const accessToken = 'MC5XcHpoVENnQUFDd0EwdG01.77-977-977-977-9S27vv70AI--_vW_vv70I77-9JWzvv70Tch3vv714C--_ve-_vUjvv73vv70-77-9Le-_vQ';
+
+    Prismic.api(apiEndpoint, {accessToken}).then(api => {
+      api.query(Prismic.Predicates.at('document.type', 'home_page')).then(response => {
+        if (response) {
+          this.setState({ doc: response.results[0] });
+        }
+      });
+    });
+  }
+
   render() {
-    return (
-      <div className="jumbotronContainer">
-        <img src={photo} alt="jumbotron image" />
-      </div>
-    );
+    console.log(this.state.doc)
+    if (this.state.doc) {
+      const document = this.state.doc.data;
+      return (
+        <div className="jumbotronContainer">
+          <img src={photo} alt="jumbotron image" />
+        </div>
+      );
+    } return (
+      <h1>
+        hello
+      </h1>
+    )
   }
 }
 
