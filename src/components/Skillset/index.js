bdf3482a388451ad49dd98e2786524c8f7dc015b
Warren Chan <warrenchan@warren-2.hitronhub.home>
2018-03-25 16:16:04 -0700

    Skill selection linked to corresponding page

--- src/components/Skillset/index.js
+++ src/components/Skillset/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styles from './styles.scss'
+import Link from 'gatsby-link'
 
 import photoicon from '../../assets/photo-icon.svg'
 import keyicon from '../../assets/key-icon.svg'
@@ -7,28 +8,64 @@ import penicon from '../../assets/pen-icon.svg'
 import bubbleSlider from '../../assets/bubble.svg'
 
 class Skillset extends Component {
-  state = {  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      skill: null,
+    }
+    this.handleOptionClick = this.handleOptionClick.bind(this);
+  }
+
+  handleOptionClick(id, event) {
+    this.setState({skill: id});
+    console.log(this.state.skill)
+  }
+
   render() {
+    const skillMessage = () => {
+      if (this.state.skill === "dev") {
+        return (
+          <div className="skillDesc">
+            <h2>I Am a Web Developer</h2>
+            <p>Front-end web developer working with you to build your personalized website. </p>
+            <Link to="/dev/">Take a look</Link>
+          </div>
+        )
+      } else if (this.state.skill === "design") {
+        return (
+          <div className="skillDesc">
+            <h2>I Am a Designer</h2>
+            <p>Creating with pen, paper, photoshop and illustrator</p>
+            <Link to="/design/">Take a look</Link>
+          </div>
+        )
+      } else {
+        return (
+          <div className="skillDesc">
+            <h2>I Am a Photographer</h2>
+            <p>Lets take some portraits, lanscape headshots or product shots. You name it.</p>
+            <Link to="/photography/">Take a look</Link>
+          </div>
+        )
+      }
+    }
+
     return (
       <div className="skillsContainer">
         <div className="sliderContainer">
           <div className="sliderBox">
-            <div className="sliderItem">
+            <div className="sliderItem" id="photography" onClick={() => (this.handleOptionClick("photography"))}>
               <img src={photoicon}/>
             </div>
-            <div className="sliderItem">
+            <div className="sliderItem" id="dev" onClick={() => (this.handleOptionClick("dev"))}>
               <img src={keyicon}/>
             </div>
-            <div className="sliderItem">
+            <div className="sliderItem" id="design" onClick={() => (this.handleOptionClick("design"))}>
               <img src={penicon}/>
             </div>
           </div>
         </div>
-        <div className="skillDesc">
-          <h2>I Am a Photographer</h2>
-          <p>Lets take some portraits, lanscape headshots or product shots. You name it.</p>
-          <a href="/404/">Take a look</a>
-        </div>
+        {skillMessage()}
       </div>
     );
   }
