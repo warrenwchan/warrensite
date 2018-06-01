11d76f54ce126ae53413fe2c229b4c3fd5024b54
Warren Chan <warrenchan@warren-2.hitronhub.home>
2018-05-31 23:39:57 -0700

    Style change

--- src/components/Skillset/index.js
+++ src/components/Skillset/index.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react'
 import styles from './styles.scss'
 import Link from 'gatsby-link'
 
-import photoicon from '../../assets/photo-icon.svg'
-import keyicon from '../../assets/key-icon.svg'
-import penicon from '../../assets/pen-icon.svg'
-import bubbleSlider from '../../assets/bubble.svg'
+import devImage from '../../assets/photos/jay-wennington-624-unsplash.jpg'
+import designImage from '../../assets/photos/brad-neathery-248309-unsplash.jpg'
+import photoImage from '../../assets/photos/fabian-grohs-524350-unsplash.jpg'
+
+const SkillBox = (props) => {
+  return (
+    <div className="skillBox" style={{ background: `linear-gradient(rgba(1, 36, 39, 0.70), rgba(21, 24, 24, 0.90)),url(${props.photo}) center / cover` }}>
+      <Link to={props.to}>
+        <div className="skillContent">
+          <h2>{props.title}</h2>
+          <p>{props.desc}</p>
+          <p>Learn more</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
 
 class Skillset extends Component {
   constructor(props) {
@@ -25,45 +38,49 @@ class Skillset extends Component {
     const skillMessage = () => {
       if (this.state.skill === "dev") {
         return (
-          <div className="skillDesc">
-            <h2>A Web Developer</h2>
-            <p>Front-end web developer working with you to build your personalized website. </p>
-            <Link to="/dev/">Take a look</Link>
-          </div>
+          <SkillBox
+            title="Web Dev"
+            desc="Front-end web developer working with you to build your personalized website."
+            to="/dev/"
+            photo={photoImage}
+          />
         )
       } else if (this.state.skill === "design") {
         return (
-          <div className="skillDesc">
-            <h2>A Graphic Design</h2>
-            <p>Creating with pen, paper, photoshop and illustrator</p>
-            <Link to="/design/">Take a look</Link>
-          </div>
+          <SkillBox
+            title="Graphic Design"
+            desc="Creating with pen, paper, photoshop and illustrator"
+            to="/design/"
+            photo={designImage}
+          />
         )
       } else {
         return (
-          <div className="skillDesc">
-            <h2>A Photographer</h2>
-            <p>Lets take some portraits, lanscape headshots or product shots. You name it.</p>
-            <Link to="/photography/">Take a look</Link>
-          </div>
+          <SkillBox
+            title="Photography"
+            desc="Lets take some portraits, lanscape headshots or product shots. You name it."
+            to="/photography/"
+            photo={devImage}
+          />
         )
       }
     }
 
     return (
       <div className="skillsContainer">
-        <div className="sliderContainer">
-          <div className="sliderBox">
-            <div className="sliderItem" id="photography" onClick={() => (this.handleOptionClick("photography"))}>
-              <img className={this.state.skill === "photography" ? "isActive" : null} src={photoicon}/>
-            </div>
-            <div className="sliderItem" id="dev" onClick={() => (this.handleOptionClick("dev"))}>
-              <img className={this.state.skill === "dev" ? "isActive" : null} src={keyicon}/>
-            </div>
-            <div className="sliderItem" id="design" onClick={() => (this.handleOptionClick("design"))}>
-              <img className={this.state.skill === "design" ? "isActive" : null} src={penicon}/>
-            </div>
-          </div>
+        <div className="skillList">
+          <a className="skillItem" onClick={() => {this.handleOptionClick('dev')}}>
+            <span className="skillItemName">Web Development</span>
+            <div className="highlight"></div>
+          </a>
+          <a className="skillItem" onClick={() => {this.handleOptionClick('design')}}>
+            <span className="skillItemName">Graphic Design</span>
+            <div className="highlight"></div>
+          </a>
+          <a className="skillItem" onClick={() => {this.handleOptionClick('photography')}}>
+            <span className="skillItemName">Photography</span>
+            <div className="highlight"></div>
+          </a>
         </div>
         {skillMessage()}
       </div>
