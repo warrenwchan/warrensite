c02b52aba8b5df918b1a67829678aea52de9bae0
Warren Chan <warrenchan@warren-2.hitronhub.home>
2018-07-10 19:40:11 -0700

    Updated skill set layout to cleaner look.

--- src/components/Skillset/index.js
+++ src/components/Skillset/index.js
@@ -8,7 +8,8 @@ import photoImage from '../../assets/photos/fabian-grohs-524350-unsplash.jpg'
 
 const SkillBox = (props) => {
   return (
-    <div className="skillBox" style={{ background: `linear-gradient(rgba(1, 36, 39, 0.70), rgba(21, 24, 24, 0.90)),url(${props.photo}) center / cover` }}>
+    <div className="skillBox">
+      <div className="skillImage icon" style={{ background: `linear-gradient(rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.5) 50%, rgba(0, 0, 0, 0.8) 90%),url(${props.photo}) center / cover` }}></div>
       <Link to={props.to}>
         <div className="skillContent">
           <h2>{props.title}</h2>
@@ -26,63 +27,29 @@ class Skillset extends Component {
     this.state = {
       skill: 'photography',
     }
-    this.handleOptionClick = this.handleOptionClick.bind(this);
-  }
-
-  handleOptionClick(id, event) {
-    this.setState({skill: id});
-    console.log(this.state.skill)
   }
 
   render() {
-    const skillMessage = () => {
-      if (this.state.skill === "dev") {
-        return (
-          <SkillBox
-            title="Web Dev"
-            desc="Front-end web developer working with you to build your personalized website."
-            to="/dev/"
-            photo={photoImage}
-          />
-        )
-      } else if (this.state.skill === "design") {
-        return (
-          <SkillBox
-            title="Graphic Design"
+    return (
+      <div className="skillsContainer">
+        <SkillBox
+          title="Web Dev"
+          desc="Front-end web developer working with you to build your personalized website."
+          to="/dev/"
+          photo={photoImage}
+        />
+        <SkillBox
+          title="Graphic Design"
             desc="Creating with pen, paper, photoshop and illustrator"
             to="/design/"
             photo={designImage}
-          />
-        )
-      } else {
-        return (
-          <SkillBox
-            title="Photography"
-            desc="Lets take some portraits, lanscape headshots or product shots. You name it."
-            to="/photography/"
-            photo={devImage}
-          />
-        )
-      }
-    }
-
-    return (
-      <div className="skillsContainer">
-        <div className="skillList">
-          <a className="skillItem" onClick={() => {this.handleOptionClick('dev')}}>
-            <span className="skillItemName">Web Development</span>
-            <div className="highlight"></div>
-          </a>
-          <a className="skillItem" onClick={() => {this.handleOptionClick('design')}}>
-            <span className="skillItemName">Graphic Design</span>
-            <div className="highlight"></div>
-          </a>
-          <a className="skillItem" onClick={() => {this.handleOptionClick('photography')}}>
-            <span className="skillItemName">Photography</span>
-            <div className="highlight"></div>
-          </a>
-        </div>
-        {skillMessage()}
+        />
+        <SkillBox
+          title="Photography"
+          desc="Lets take some portraits, lanscape headshots or product shots. You name it."
+          to="/photography/"
+          photo={devImage}
+        />
       </div>
     );
   }
